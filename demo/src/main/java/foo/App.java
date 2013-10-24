8b3963f9c2c55ce3250c71430d78f6e9de987919
vijain <vijain@connecture.com>
2013-10-24 18:23:19 +0530

    commit 3

--- demo/src/main/java/foo/App.java
+++ demo/src/main/java/foo/App.java
@@ -12,7 +12,8 @@ public class App
         System.out.println( "Hello World! Stash" );
          System.out.println( "Hello World! test1" );
         System.out.println( "Hello World! test2" );
-        System.out.println( "Hello World! mofigy from GitHUb" );
+        System.out.println( "Hello World! mofigy from GitHUb1.1" );
         System.out.println( "Hello World! mofigy from Tag2" );
+        System.out.println( "Hello World! mofigy from Tag2.1" );
     }
 }
