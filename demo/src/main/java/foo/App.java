782c5dc6cb00085eb228bada3d1ec47a1dcaf175
vijain <vijain@connecture.com>
2013-10-24 16:38:14 +0530

    commit in test1

--- demo/src/main/java/foo/App.java
+++ demo/src/main/java/foo/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! Stash" );
+        System.out.println( "Hello World! test1" );
     }
 }
