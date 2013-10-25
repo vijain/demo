def2b5fadba15643742877e2d76425a95a71076e
vijain <vijain@connecture.com>
2013-10-25 11:53:33 +0530

    Rebase 1

--- demo/src/main/java/foo/App.java
+++ demo/src/main/java/foo/App.java
@@ -10,10 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! Stash" );
-         System.out.println( "Hello World! test1" );
-        System.out.println( "Hello World! test2" );
-        System.out.println( "Hello World! mofigy from GitHUb1.1" );
-        System.out.println( "Hello World! mofigy from Tag2" );
-        System.out.println( "Hello World! mofigy from Tag2.1" );
+         
     }
 }
